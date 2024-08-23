ff041ed1c684eae8c59f46cb6a51caade5c88721
SCOTTWO5 <scottidris6@gmail.com>
2024-08-23 21:30:54 +0200

    created my first project with javascript

---
+++ intro.js
@@ -0,0 +1,13 @@
+let width = 4;
+let height = 5;
+
+console.log(`Width: ${width} cm`);
+console.log(`Height: ${height} cm`);
+
+// area = width * height
+let area = width * height;
+console.log(`Area: ${area} cm2`);
+
+// perimeter = 2 * (width + heght)
+let perimeter = 2 * (width + height);
+console.log(`Perimeter: ${perimeter} cm`);
