216841531cf996b6bc1bb416fd9878b39b86d394
SCOTTWO5 <scottidris6@gmail.com>
2024-08-30 01:26:22 +0200

    learnt for loops

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(fruits);
+console.log(numbers);
+
+console.log(fruits[3]);
+console.log(numbers[3]);
---
+++ inventory.js
@@ -0,0 +1,12 @@
+let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry', 'kiwi', 'grape', 'watermelon', 'pineapple', 'blueberry'];
+let vegetable = ['carrot', 'tomato', 'pepper', 'lettuce'];
+let fastFood = ['hamburger', 'fries', 'hotdog', 'pizza'];
+
+console.log("==== Inventory Management System ====");
+console.log("---- fruits ----");
+for (let i = 0; i < fruits.length; i++){
+    let j = i + 1;
+    console.log(' ' + j + '. ' + fruits[i]);
+}
+console.log("-----------------");
+console.log("======================================")
