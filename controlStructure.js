017438efd5ce32ea2782849b8f198843265060c9
SCOTTWO5 <scottidris6@gmail.com>
2024-08-29 22:07:00 +0200

    first part of control structures

---
+++ controlStructure.js
@@ -0,0 +1,15 @@
+// if-slse control structure
+let age = 17;
+
+console.log(`You are ${age} years old`);
+
+if (age >= 18 && age <= 30){
+    console.log("Get your ass up and go vote nigga");
+
+} else if(age > 30){
+    console.log("I told you to get out you too damn old, you over 30 nigga")
+}
+
+else{
+    console.log("Hell nah nigga, get your ass out of here kid");
+}
---
+++ operators.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+let a = 10;
+let b = 5;
+let result = a + b;
+console.log(result); // 15
+
+// Compound Assignment Operator
+let x = 10;
+x +=5;
+console.log; // 15
+
+// Comparison Operator
+let isEqual = (a == b);
+console.log(isEqual); // false
+
+// Logical Operator
+let logicalResult = (a > 5 && b < 10);
+console.log(logicalResult); // true
+
+// String Operator
+let fullName = "John" + " " + "Doe";
+console.log(fullName); // John Doe
+
+// Terinary Operator
+let max = (a < b) ? a : b;
+console.log(max); // 10
