b368179af5e3b0471092533f7b9ba42d66eb2c8b
SCOTTWO5 <scottidris6@gmail.com>
2024-08-30 01:46:11 +0200

    learnt while loops

--- inventory.js
+++ inventory.js
@@ -1,5 +1,5 @@
 let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry', 'kiwi', 'grape', 'watermelon', 'pineapple', 'blueberry'];
-let vegetable = ['carrot', 'tomato', 'pepper', 'lettuce'];
+let vegetable    = ['carrot', 'tomato', 'pepper', 'lettuce', 'cucumber', 'broccoli', 'onion', 'garlic', 'potato', 'ginger'];
 let fastFood = ['hamburger', 'fries', 'hotdog', 'pizza'];
 
 console.log("==== Inventory Management System ====");
@@ -9,4 +9,14 @@ for (let i = 0; i < fruits.length; i++){
     console.log(' ' + j + '. ' + fruits[i]);
 }
 console.log("-----------------");
+
+console.log("---- Vegetable ----");
+let n = 0;
+while (n < vegetable.length){
+    let m = n + 1;
+    console.log(' ' + m + '. ' + vegetable[n]);
+    n++;
+}
+console.log("--------------------")
+
 console.log("======================================")
