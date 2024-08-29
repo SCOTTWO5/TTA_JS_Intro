0d5917821dc8b100433bb5f2d9b3e0c5b8c56c34
SCOTTWO5 <scottidris6@gmail.com>
2024-08-29 22:22:32 +0200

    Understood control structure

---
+++ switch.js
@@ -0,0 +1,22 @@
+let day = 'Sunday';
+
+switch(day){
+    case 'Monday':
+        console.log('Today you work onsite');
+        break;
+    case 'Tuesday':
+        console.log('Today you work remotely');
+        break;
+    case 'Wednesday':
+        console.log('Today you have a work break');
+        break;
+    case 'Thursday':
+        console.log('Today you work onsite');
+        break;
+    case 'Friday':
+        console.log('Today you work remotely');
+        break;
+    default:
+        console.log("Today is't a work day");
+        break;
+}
