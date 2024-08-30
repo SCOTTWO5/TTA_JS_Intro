94d304f259f2bbfc7395c5377a1cf41f82263862
SCOTTWO5 <scottidris6@gmail.com>
2024-08-30 02:14:20 +0200

    completed the inventory management system

--- inventory.js
+++ inventory.js
@@ -1,6 +1,6 @@
 let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry', 'kiwi', 'grape', 'watermelon', 'pineapple', 'blueberry'];
 let vegetable    = ['carrot', 'tomato', 'pepper', 'lettuce', 'cucumber', 'broccoli', 'onion', 'garlic', 'potato', 'ginger'];
-let fastFood = ['hamburger', 'fries', 'hotdog', 'pizza'];
+let fastFood = ['hamburger', 'fries', 'hotdog', 'pizza', 'ice cream', 'chicken nuggets', 'sandwich', 'onion rings', 'taco', 'burrito',];
 
 console.log("==== Inventory Management System ====");
 console.log("---- fruits ----");
@@ -9,6 +9,7 @@ for (let i = 0; i < fruits.length; i++){
     console.log(' ' + j + '. ' + fruits[i]);
 }
 console.log("-----------------");
+console.log(`Total number of fruits: ${fruits.length}`);
 
 console.log("---- Vegetable ----");
 let n = 0;
@@ -17,6 +18,13 @@ while (n < vegetable.length){
     console.log(' ' + m + '. ' + vegetable[n]);
     n++;
 }
-console.log("--------------------")
+console.log("--------------------");
+console.log(`Total number of vegetation: ${vegetable.length}`);
 
+console.log("---- Fast Food ----");
+fastFood.forEach((food, index) => {
+    console.log(' ' + (index + 1) + '. ' + food);
+});
+console.log("--------------------");
+console.log(`Total number of fast food: ${fastFood.length}`);
 console.log("======================================")
